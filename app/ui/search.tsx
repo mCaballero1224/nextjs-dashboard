06eb7ef7b16a992499c1f439937b693bc5d7da95
mCaballero1224 <mcaballero.dev@gmail.com>
2026-03-08 12:28:50 +0900

    Implement debouncing on invoice search function

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,14 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebounceCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
 	const searchParams = useSearchParams();
 	const pathname = usePathname();
 	const { replace } = useRouter();
 
-	function handleSearch(term: string) {
+	const handleSearch = useDebounceCallback((term) => {
 		const params = new URLSearchParams(searchParams);
 		if (term) { /* set params string based on user input */
 			params.set('query', term)
@@ -19,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 		 * and update URL with user's search data
 		 */
 		replace(`${pathname}?${params.toString()}`);
-	}
+	}, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
