ea44a1593c2b2e80d4c0ea8fcd89d71d95fa51b7
mCaballero1224 <mcaballero.dev@gmail.com>
2026-03-08 12:17:09 +0900

    Implement search functionality to invoices page

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,24 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const { replace } = useRouter();
+
 	function handleSearch(term: string) {
-		console.log(term);
+		const params = new URLSearchParams(searchParams);
+		if (term) { /* set params string based on user input */
+			params.set('query', term)
+		} else { /* delete the params if user input is empty */
+			params.delete('query')
+		}
+		/* translate input into URL-friendly format
+		 * and update URL with user's search data
+		 */
+		replace(`${pathname}?${params.toString()}`);
 	}
 
   return (
@@ -18,6 +32,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 				onChange={(e) => {
 					handleSearch(e.target.value); /* capture user input */
 				}}
+				defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
