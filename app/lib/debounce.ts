d30618a3b3f6861f0d3532f33d8da2af9adf246d
mCaballero1224 <mcaballero.dev@gmail.com>
2026-03-08 21:24:52 +0900

    Add types to debounce function

--- app/lib/debounce.ts
+++ app/lib/debounce.ts
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: string, delay: int) => {
 	const [debouncedValue, setDebouncedValue] = useState(value);
 
 	useEffect(() => {
